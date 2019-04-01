b21dacdfb3cfcac7c3fd18da4cf2e8c63c56179a
Michal Chorazak <32418957+mchorazak@users.noreply.github.com>
2019-04-01 16:30:24 +0100

    Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -6,7 +6,7 @@ class spdlogConan(ConanFile):
     name = "spdlog-graylog"
     libname = "spdlog"
     src_version = "1.2.1"
-    version = "1.2.1-dm7"
+    version = "1.2.1-dm8"
     homepage = "https://github.com/ess-dmsc/spdlog"
     license = "BSD 2-Clause"
     url = "https://github.com/ess-dmsc/conan-spdlog"
